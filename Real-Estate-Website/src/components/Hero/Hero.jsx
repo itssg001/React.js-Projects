1e8e2fc32246c1fdd2218afb2e8f03a765258a30
Sujit Ghogare <116686235+itssg001@users.noreply.github.com>
2024-05-17 00:21:21 +0530

    Hero Section created

--- Real-Estate-Website/src/components/Hero/Hero.jsx
+++ Real-Estate-Website/src/components/Hero/Hero.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { AiFillEnvironment } from "react-icons/ai";
+import CountUp from "react-countup";
 import "./hero.css";
 const Hero = () => {
   return (
@@ -6,12 +8,54 @@ const Hero = () => {
       <div className="hero-container">
         {/* left-side  */}
         <div className="hero-left">
-          <div className="hero-title">
+          <div className="hero-tittle">
+          <div className="orange-circle"></div>
             <h1>
               Discover <br />
               Most Suitble <br />
               Property
             </h1>
+          </div>
+          <div className=" secondaryText hero-des">
+            <span>Find a variety of properties that suit you very easily</span>
+            <span>Forget all difficulties in finding residence for you</span>
+          </div>
+          <div className="search-bar">
+          <AiFillEnvironment color="var(--blue)" size={25} />
+          <input type="text" />
+          <button className="button">search</button>
+          </div>
+          <div className="stats">
+            <div className=" stat">
+                <span>
+                    <CountUp start={8800} end={9000} duration={4} />
+                    <span>+</span>
+                </span>
+               
+                <span className="secondaryText">
+                    Premium Products
+                </span>
+            </div>
+            <div className=" stat">
+                <span>
+                    <CountUp start={1950} end={2000} duration={4} />
+                    <span>+</span>
+                </span>
+               
+                <span className="secondaryText">
+                    Happy Customers
+                </span>
+            </div>
+            <div className=" stat">
+                <span>
+                    <CountUp start={0} end={28}  />
+                    <span>+</span>
+                </span>
+               
+                <span className="secondaryText">
+                    Award Winnings
+                </span>
+            </div>
           </div>
         </div>
 
