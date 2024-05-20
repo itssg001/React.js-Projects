e498ca4fe3952e86365d887c1317fa79d0c8209e
Sujit Ghogare <116686235+itssg001@users.noreply.github.com>
2024-05-20 16:16:41 +0530

    get started completed

--- Real-Estate-Website/src/components/GetStarted/GetStarted.jsx
+++ Real-Estate-Website/src/components/GetStarted/GetStarted.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import './getstarted.css';
 const GetStarted = () => {
   return (
-    <div>
-      Get Started
-    </div>
+    <section className="g-wrapper">
+      <div className="paddings g-container">
+        <div className="flexColCenter inner-container">
+          <span className='primaryText'>Get started with Homyz</span>
+          <span className='secondaryText'>Subscribe and find super attractive price quotes
+          <br />
+          Find Your Residence Soon
+          </span>
+          <button className='button'>
+            <a href="mailto:spghogare456@gmail.com">Get Started</a>
+          </button>
+        </div>
+      </div>
+    </section>
   )
 }
 
